740e4270edc4ca66fbfc7151191ed1c319abba39
Manu Prathap <manuprathap@Manus-MacBook-Pro.local>
2023-09-09 22:40:24 +0530

    Initial Commit

---
+++ Assets/Scripts/ObjectParser.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class ObjectParser : MonoBehaviour
+{
+    private Template objectTemplate;
+    public string fileName = "Assets/Data/JSON/object.json";
+
+    void Start()
+    {
+        string jsonString = File.ReadAllText(fileName);
+        objectTemplate = CreateFromJSON(jsonString);
+        CreateObjectStructure();
+    }
+
+    public static Template CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<Template>(jsonString);
+    }
+
+
+    private void CreateObjectStructure()
+    {
+        foreach (var template in objectTemplate.objectHierarchy)
+        {
+            CreateGameObject(template);
+        }
+    }
+
+    private GameObject CreateGameObject(TransformStruct transform)
+    {
+        GameObject parent = new GameObject(transform.name);
+        SetTransformation(parent, transform);
+
+        foreach (var childTransform in transform.Children)
+        {
+            var childGO = CreateGameObject(childTransform);
+            childGO.transform.parent = parent.transform;
+
+        }
+        return parent;
+    }
+
+
+    private void SetTransformation(GameObject gameObject, TransformStruct transform)
+    {
+        gameObject.transform.position = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        gameObject.transform.rotation = new Quaternion(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z, transform.localRotation.w);
+        gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+    }
+
+}
+
